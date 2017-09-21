bf5893efab1872da237b547ba5051ac6de1fcca2
niawkung <niawkung2@gmail.com>
2017-09-21 09:54:38 +0700

    add state 0-9

---
+++ state0.js
@@ -0,0 +1,8 @@
+demo.state0 = function(){};
+demo.state0.prototype = {
+    preload: function(){},
+    create: function(){
+         game.stage.backgroundColor = '#80ff80';
+    },
+    update: function(){}
+};
