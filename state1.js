d0898b5f353c7f0a0a1767b7f33164d3c7d89a1a
niawkung <niawkung2@gmail.com>
2017-09-22 09:55:44 +0700

    add animation and map

--- state1.js
+++ state1.js
@@ -1,10 +1,22 @@
 demo.state1 = function(){};
 demo.state1.prototype = {
-    preload: function(){},
+  preload: function(){
+    game.load.tilemap('field', 'assets/tilemaps/field.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('grassTiles', 'assets/tilemaps/grassTiles.png');
+    game.load.image('rockTiles', 'assets/tilemaps/rockTiles.png');
+    game.load.image('adam', 'assets/sprites/adam.png');
+  },
     create: function(){
-        game.stage.backgroundColor = '#DDDDDD';
-        console.log('state1');
-        addChangeStateEventListeners();
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+		game.stage.backgroundColor = '#DDDDDD';
+		addChangeStateEventListeners();
+
+		var map = game.add.tilemap('field');
+		map.addTilesetImage('grassTiles');
+		map.addTilesetImage('rockTiles');
+
+		grass = map.createLayer('grass');
+		rocks = map.createLayer('rocks');
     },
     update: function(){}
 };
